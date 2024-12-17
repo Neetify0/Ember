a01043bce62e5898414d9bb846d13cfcee55b03f
Neetify0 <navaneetboddepalli@gmail.com>
2024-12-16 20:18:27 -0500

    Added Python automation script

--- add.py
+++ add.py
@@ -1,4 +1,24 @@
-<!DOCTYPE html>
+import os
+
+directories = [
+    r'C:\Users\Namel\OneDrive\Documents\Ember\games',
+    r'C:\Users\Namel\OneDrive\Documents\Ember\apps'
+]
+
+def get_game_folders(directory):
+    game_folders = []
+    for folder in os.listdir(directory):
+        folder_path = os.path.join(directory, folder)
+        if os.path.isdir(folder_path):
+            game_folders.append(folder)
+    return game_folders
+
+def capitalize_title(title):
+    return ' '.join([word.capitalize() for word in title.split('_')])
+
+def remake_index_html(directory, game_folders):
+    index_html_path = os.path.join(directory, 'index.html')
+    html_content = f"""<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -22,17 +42,24 @@
     </nav>
 
     <div class="center_content">
-        <h1>Apps</h1>
+        <h1>{directory.split(os.sep)[-1].capitalize()}</h1>
         <h2 id="random-text"></h2>
     </div>
 
-    <div class="tile-grid">
+    <div class="tile-grid">"""
+    
+    for game_folder in game_folders:
+        game_name = capitalize_title(game_folder)
+        image_path = f"/images/{game_folder}.png"
+        html_content += f"""
         <div class="tile">
-            <a href="/apps/cobalt/">
-                <img src="/images/cobalt.png" alt="Cobalt">
-                <div class="tile-title">Cobalt</div>
+            <a href="/{directory.split(os.sep)[-1]}/{game_folder}/">
+                <img src="{image_path}" alt="{game_name}">
+                <div class="tile-title">{game_name}</div>
             </a>
-        </div>
+        </div>"""
+    
+    html_content += """
     </div>
 
     <footer>
@@ -62,4 +89,11 @@
     </script>
 
 </body>
-</html>
+</html>"""
+    
+    with open(index_html_path, 'w', encoding='utf-8') as file:
+        file.write(html_content)
+
+for directory in directories:
+    game_folders = get_game_folders(directory)
+    remake_index_html(directory, game_folders)
